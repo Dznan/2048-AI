413600b4196ea007ed732325c7395bce437adc95
gjh16337068 <2372635457@qq.com>
2019-07-07 22:30:53 +0800

    Add files via upload

---
+++ src/UI.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import pygame
+from pygame.locals import *
+ 
+PIXEL = 150
+SCORE_PIXEL = 100
+SIZE = 4
+
+# 更新屏幕
+def show(game, screen, block, map_font, score_font, score_block):
+    for i in range(SIZE):
+        for j in range(SIZE):
+            # 背景颜色块
+            screen.blit(game.state[i,j] == 0 and block[(i + j) % 2] or block[2 + (i + j) % 2], (PIXEL * j, PIXEL * i))
+            # 数值显示
+            if game.state[i,j] != 0:
+                map_text = map_font.render(str(game.state[i,j]), True, (106, 90, 205))
+                text_rect = map_text.get_rect()
+                text_rect.center = (PIXEL * j + PIXEL / 2, PIXEL * i + PIXEL / 2)
+                screen.blit(map_text, text_rect)
+    # 分数显示
+    screen.blit(score_block, (0, PIXEL * SIZE))
+    score_text = score_font.render((game.is_terminal() and "Game over with score " or "Score: ") + str(game.score), True, (106, 90, 205))
+    score_rect = score_text.get_rect()
+    score_rect.center = (PIXEL * SIZE / 2, PIXEL * SIZE + SCORE_PIXEL / 2)
+    screen.blit(score_text, score_rect)
+    pygame.display.update()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
-
+from UI import *
 from game2048env import Game2048Env
-
-
+import pygame
+from pygame.locals import *
+import time
 demo_state = np.array([
     [2, 2, 4, 0],
     [2, 2, 4, 0],
@@ -11,23 +12,42 @@ demo_state = np.array([
     [2, 2, 4, 0]
 ])
 
-
 def main(argv):
     env = Game2048Env()
     env.init(state=demo_state)
+    
+    pygame.init()
+    screen = pygame.display.set_mode((PIXEL * SIZE, PIXEL * SIZE + SCORE_PIXEL))
+    pygame.display.set_caption("2048")
+    block = [pygame.Surface((PIXEL, PIXEL)) for i in range(4)]
+    # 设置颜色
+    block[0].fill((152, 251, 152))
+    block[1].fill((240, 255, 255))
+    block[2].fill((0, 255, 127))
+    block[3].fill((225, 255, 255))
+    score_block = pygame.Surface((PIXEL * SIZE, SCORE_PIXEL))
+    score_block.fill((245, 245, 245))
+    # 设置字体
+    map_font = pygame.font.Font(None, PIXEL * 2 // 3)
+    score_font = pygame.font.Font(None, SCORE_PIXEL * 2 // 3)
+    clock = pygame.time.Clock()
+    show(env, screen, block, map_font, score_font, score_block)
+
     while not env.is_terminal():
+        clock.tick(2)
         env.render()
         actions = env.action_space
         if env.turn == 'MOVE':
             print('Actions:')
             for i, a in enumerate(actions):
-                print('{}: {}'.format(i, a), end='; ')
-            print()
-            pick = int(input('Input action id: '))
+                print('{}: {}'.format(i, a))
+            pick = random.randint(0, len(actions)-1)#int(input('Input action id: '))
         else:
             pick = np.random.randint(len(actions))
         state, done, reward, info = env.step(actions[pick])
+        show(env, screen, block, map_font, score_font, score_block)
 
 
 if __name__ == '__main__':
     main(sys.argv)
+    time.sleep(5)
