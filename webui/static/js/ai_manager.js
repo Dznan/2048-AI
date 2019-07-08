b6e997b8cc2f4e9c347761700b00858b9b460532
某罐头菠萝 <heglan@mail2.sysu.edu.cn>
2019-07-08 20:51:17 +0800

    fixed bug of webui

--- webui/static/js/ai_manager.js
+++ webui/static/js/ai_manager.js
@@ -1,4 +1,4 @@
-var sleep = async (duration) => {
+var sleep = async(duration) => {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, duration);
     });
@@ -9,88 +9,87 @@ var stopAuto = true;
 function AIManager() {
     this.events = {};
     this.listen();
-  }
-  
-  AIManager.prototype.on = function (event, callback) {
+}
+
+AIManager.prototype.on = function(event, callback) {
     if (!this.events[event]) {
-      this.events[event] = [];
+        this.events[event] = [];
     }
     this.events[event].push(callback);
-  };
-  
-  AIManager.prototype.emit = function (event, data) {
+};
+
+AIManager.prototype.emit = function(event, data) {
     var callbacks = this.events[event];
     if (callbacks) {
-      callbacks.forEach(function (callback) {
-        callback(data);
-      });
+        callbacks.forEach(function(callback) {
+            callback(data);
+        });
     }
-  };
-  
-  AIManager.prototype.listen = function () {
+};
+
+AIManager.prototype.listen = function() {
     var self = this;
 
     // // Respond to button presses
     this.bindButtonPress(".nextstep-button", this.nextstep);
     this.bindButtonPress(".autoplay-button", this.autoplay);
     // this.bindButtonPress(".keep-playing-button", this.keepPlaying);
-  
+
     // if (Math.max(absDx, absDy) > 10) {
     // // (right : left) : (down : up)
     // self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
     // }
-  };
-  
-  AIManager.prototype.nextstep = async function (event) {
+};
+
+AIManager.prototype.nextstep = async function(event) {
     event.preventDefault();
     var self = this;
-    if(game.isGameTerminated()) return;
+    if (game.isGameTerminated()) return;
     let grid = game.grid.to_json();
     var postData = new URLSearchParams({
         "grid": JSON.stringify(grid),
     });
     await fetch("/api/nextstep", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: postData
-    })
-    .then(Response => Response.json())
-    .then(res => {
-        let actionList = {
-            "U" : 0,
-            "R" : 1,
-            "D" : 2,
-            "L" : 3
-        };
-        self.emit("move", actionList[res.action]);
-    });
-  };
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: postData
+        })
+        .then(Response => Response.json())
+        .then(res => {
+            let actionList = {
+                "U": 0,
+                "R": 1,
+                "D": 2,
+                "L": 3
+            };
+            self.emit("move", actionList[res.action]);
+        });
+};
 
-  AIManager.prototype.autoplay = async function (event) {
+AIManager.prototype.autoplay = async function(event) {
     event.preventDefault();
     var self = this;
     stopAuto = !stopAuto;
-    if (stopAuto) 
+    if (stopAuto)
         document.querySelector(".autoplay-button").innerHTML = "Auto Play";
     else
         document.querySelector(".autoplay-button").innerHTML = "Stop";
-    if (stopAuto) return; 
-    while(!game.isGameTerminated()) {
+    if (stopAuto) return;
+    while (!game.isGameTerminated()) {
         if (stopAuto) break;
-        self.nextstep(event);
+        await self.nextstep(event);
         await sleep(100);
     }
     if (!stopAuto) {
-      stopAuto = !stopAuto;
-      document.querySelector(".autoplay-button").innerHTML = "Auto Play";
+        stopAuto = !stopAuto;
+        document.querySelector(".autoplay-button").innerHTML = "Auto Play";
     }
-  };
-  
-  AIManager.prototype.bindButtonPress = function (selector, fn) {
+};
+
+AIManager.prototype.bindButtonPress = function(selector, fn) {
     var button = document.querySelector(selector);
     button.addEventListener("click", fn.bind(this));
     button.addEventListener(this.eventTouchend, fn.bind(this));
-  };
-  
+};
