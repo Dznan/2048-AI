cfbfc9bffc14cc7b771dc644fb9cd91ef38c93f3
Dznan <tsenantung@yahoo.com>
2019-07-09 00:16:42 +0800

    Minor updates in eval_func

--- webui/app.py
+++ webui/app.py
@@ -13,8 +13,49 @@ from game2048env import Game2048Env
 from minimax import MiniMaxPlayer
 
 
-def eval_func(state):
-    return np.sum(state) + len(state[state == 0]) * np.average(state)
+def avg_dis(state):
+    def dis(a, b):
+        return np.abs(a[0] - b[0]) + np.abs(a[1] - b[1])
+    s = {}
+    d = 0.
+    height, width = state.shape
+    tiles = np.unique(state)
+    tiles = np.sort(tiles)
+    for tile in tiles:
+        s[tile] = []
+    for i in range(height):
+        for j in range(width):
+            s[state[i, j]].append((i, j))
+    
+    for k in range(len(tiles)):
+        d_ = 0.
+        for i in range(len(s[tiles[k]])):
+            for j in range(i):
+                d_ += dis(s[tiles[k]][i], s[tiles[k]][j])
+
+        if k > 0:
+            for i in range(len(s[tiles[k]])):
+                for j in range(len(s[tiles[k-1]])):
+                    d_ += dis(s[tiles[k]][i], s[tiles[k-1]][j])
+        
+        d += np.log2(tiles[k] + 1) * d_
+    return d
+
+
+def eval_func(env):
+    max_tile = np.max(env.state)
+    score = env.score
+    empty_tiles = len(env.state[env.state == 0])
+    adis = avg_dis(env.state)
+    return max_tile + 0.5 * score + empty_tiles * np.average(env.state) - 0.5 * adis
+
+
+action_map = {
+    'DOWN': 'D',
+    'LEFT': 'L',
+    'UP': 'U',
+    'RIGHT': 'R',
+}
 
 
 def ai_func(grid):
@@ -25,21 +66,12 @@ def ai_func(grid):
     返回:
         0 移动方向(U L D R)
     """
-    action_map = {
-        'DOWN': 'D',
-        'LEFT': 'L',
-        'UP': 'U',
-        'RIGHT': 'R',
-    }
+
     env = Game2048Env(init_state=np.array(grid))
-    player = MiniMaxPlayer(eval_func, max_depth=3)
+    player = MiniMaxPlayer(eval_func, max_depth=5, max_child=4)
 
     action = player.choose_action(env)
     print(action)
-
-    # rndList = ["U", "L", "D", "R"]
-    # random.shuffle(rndList)
-    # return rndList[0]
 
     return action_map[action]
     
--- webui/minimax.py
+++ webui/minimax.py
@@ -1,7 +1,11 @@
+import numpy as np
+
+
 class MiniMaxPlayer:
-    def __init__(self, eval_func, max_depth=20):
+    def __init__(self, eval_func, max_depth=20, max_child=None):
         self.eval_func = eval_func
         self.max_depth = max_depth
+        self.max_child = max_child
         self.NINF = -1e9
         self.INF = 1e9
     
@@ -11,10 +15,14 @@ class MiniMaxPlayer:
     
     def minimize(self, environment, alpha, beta, depth):
         if depth >= self.max_depth or environment.is_terminal():
-            return None, self.eval_func(environment.state)
+            return None, self.eval_func(environment)
 
         min_action, min_value = None, 1e9
         actions = environment.action_space
+        if self.max_child is not None and len(actions) > self.max_child:
+            np.random.shuffle(actions)
+            actions = actions[:self.max_child]
+
         for action in actions:
             child = environment.copy()
             child.step(action)
@@ -30,10 +38,14 @@ class MiniMaxPlayer:
     
     def maximize(self, environment, alpha, beta, depth):
         if depth >= self.max_depth or environment.is_terminal():
-            return None, self.eval_func(environment.state)
+            return None, self.eval_func(environment)
         
         max_action, max_value = None, -1e9
         actions = environment.action_space
+        if self.max_child is not None and len(actions) > self.max_child:
+            np.random.shuffle(actions)
+            actions = actions[:self.max_child]
+
         for action in actions:
             child = environment.copy()
             child.step(action)
