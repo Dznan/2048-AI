758e3747b26f8e44bbd25ad533bbdf85183255da
Dznan <tsenantung@yahoo.com>
2019-07-08 16:25:03 +0800

    Update minimax.py
    Add alpha-beta pruning

--- webui/minimax.py
+++ webui/minimax.py
@@ -2,12 +2,14 @@ class MiniMaxPlayer:
     def __init__(self, eval_func, max_depth=20):
         self.eval_func = eval_func
         self.max_depth = max_depth
+        self.NINF = -1e9
+        self.INF = 1e9
     
     def choose_action(self, environment):
-        child, _ = self.maximize(environment, 0)
+        child, _ = self.maximize(environment, self.NINF, self.INF, 0)
         return child
     
-    def minimize(self, environment, depth):
+    def minimize(self, environment, alpha, beta, depth):
         if depth >= self.max_depth or environment.is_terminal():
             return None, self.eval_func(environment.state)
 
@@ -16,13 +18,17 @@ class MiniMaxPlayer:
         for action in actions:
             child = environment.copy()
             child.step(action)
-            _, value = self.maximize(child, depth+1)
+            _, value = self.maximize(child, alpha, beta, depth+1)
             if value < min_value:
                 min_action, min_value = action, value
+            if min_value <= alpha:
+                break
+            if min_value < beta:
+                beta = min_value
         
         return min_action, min_value
     
-    def maximize(self, environment, depth):
+    def maximize(self, environment, alpha, beta, depth):
         if depth >= self.max_depth or environment.is_terminal():
             return None, self.eval_func(environment.state)
         
@@ -31,8 +37,12 @@ class MiniMaxPlayer:
         for action in actions:
             child = environment.copy()
             child.step(action)
-            _, value = self.minimize(child, depth+1)
+            _, value = self.minimize(child, alpha, beta, depth+1)
             if value > max_value:
                 max_action, max_value = action, value
+            if max_value >= beta:
+                break
+            if max_value > alpha:
+                alpha = max_value
         
         return max_action, max_value
