43ec895a0d513faa5987d4c4a42d7ad0e5fa3507
Dznan <tsenantung@yahoo.com>
2019-07-09 09:53:02 +0800

    Update minimax.py pruning

--- webui/app.py
+++ webui/app.py
@@ -42,12 +42,37 @@ def avg_dis(state):
     return d
 
 
+def get_greatest_exp_of_2(n):
+    i = 2
+    while i <= n:
+        i <<= 1
+    return i >> 1
+
+
+def get_count(state):
+    s = np.sum(state)
+    c = 0
+    res = {}
+    while s > 0:
+        n = get_greatest_exp_of_2(s)
+        if n not in res:
+            res[n] = 1
+        else:
+            res[n] += 1
+        s -= n
+        c += 1
+    return res, c
+
+
 def eval_func(env):
     max_tile = np.max(env.state)
     score = env.score
+    tiles = len(env.state[env.state != 0])
     empty_tiles = len(env.state[env.state == 0])
     adis = avg_dis(env.state)
-    return max_tile + 0.5 * score + empty_tiles * np.average(env.state) - 0.5 * adis
+    stat, c = get_count(env.state)
+    # return max_tile + 0.5 * score + 0.5 * empty_tiles * np.average(env.state) - adis
+    return max_tile + (c - tiles) * np.average(env.state)
 
 
 action_map = {
@@ -68,10 +93,10 @@ def ai_func(grid):
     """
 
     env = Game2048Env(init_state=np.array(grid))
-    player = MiniMaxPlayer(eval_func, max_depth=5, max_child=4)
+    player = MiniMaxPlayer(eval_func, max_depth=7, max_child=4)
 
     action = player.choose_action(env)
-    print(action)
+    print(action, get_count(np.array(grid)))
 
     return action_map[action]
     
--- webui/minimax.py
+++ webui/minimax.py
@@ -2,10 +2,12 @@ import numpy as np
 
 
 class MiniMaxPlayer:
-    def __init__(self, eval_func, max_depth=20, max_child=None):
+    def __init__(self, eval_func, max_depth=20, max_child=None, prune_mini_step=False, prune_max_step=False):
         self.eval_func = eval_func
         self.max_depth = max_depth
         self.max_child = max_child
+        self.prune_mini_step = prune_mini_step
+        self.prune_max_step = prune_max_step
         self.NINF = -1e9
         self.INF = 1e9
     
@@ -16,24 +18,34 @@ class MiniMaxPlayer:
     def minimize(self, environment, alpha, beta, depth):
         if depth >= self.max_depth or environment.is_terminal():
             return None, self.eval_func(environment)
-
+                
         min_action, min_value = None, 1e9
         actions = environment.action_space
-        if self.max_child is not None and len(actions) > self.max_child:
-            np.random.shuffle(actions)
-            actions = actions[:self.max_child]
 
-        for action in actions:
-            child = environment.copy()
-            child.step(action)
-            _, value = self.maximize(child, alpha, beta, depth+1)
-            if value < min_value:
-                min_action, min_value = action, value
-            if min_value <= alpha:
-                break
-            if min_value < beta:
-                beta = min_value
-        
+        if self.prune_mini_step:
+            for action in actions:
+                child = environment.copy()
+                child.step(action)
+                value = self.eval_func(child)
+
+                if value < min_value:
+                    min_action, min_value = action, value
+        else:  
+            if self.max_child is not None and len(actions) > self.max_child:
+                np.random.shuffle(actions)
+                actions = actions[:self.max_child]
+
+            for action in actions:
+                child = environment.copy()
+                child.step(action)
+                _, value = self.maximize(child, alpha, beta, depth+1)
+                if value < min_value:
+                    min_action, min_value = action, value
+                if min_value <= alpha:
+                    break
+                if min_value < beta:
+                    beta = min_value
+
         return min_action, min_value
     
     def maximize(self, environment, alpha, beta, depth):
@@ -42,19 +54,28 @@ class MiniMaxPlayer:
         
         max_action, max_value = None, -1e9
         actions = environment.action_space
-        if self.max_child is not None and len(actions) > self.max_child:
-            np.random.shuffle(actions)
-            actions = actions[:self.max_child]
+        if self.prune_max_step:
+            for action in actions:
+                child = environment.copy()
+                child.step(action)
+                value = self.eval_func(child)
 
-        for action in actions:
-            child = environment.copy()
-            child.step(action)
-            _, value = self.minimize(child, alpha, beta, depth+1)
-            if value > max_value:
-                max_action, max_value = action, value
-            if max_value >= beta:
-                break
-            if max_value > alpha:
-                alpha = max_value
-        
+                if value > max_value:
+                    max_action, max_value = action, value
+        else:
+            if self.max_child is not None and len(actions) > self.max_child:
+                np.random.shuffle(actions)
+                actions = actions[:self.max_child]
+
+            for action in actions:
+                child = environment.copy()
+                child.step(action)
+                _, value = self.minimize(child, alpha, beta, depth+1)
+                if value > max_value:
+                    max_action, max_value = action, value
+                if max_value >= beta:
+                    break
+                if max_value > alpha:
+                    alpha = max_value
+            
         return max_action, max_value
